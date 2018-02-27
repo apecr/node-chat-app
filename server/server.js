effc891b4fcdcae976ebcdf12e1ed925d2cd4770
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-27 16:28:33 +0100

    feat() Include socket.io
    Include socket.io both in the client and server
    
    S9,L107

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.listen(PORT, () => console.log(`Started on port ${PORT}`));
+
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+server.listen(PORT, () => console.log(`Started on port ${PORT}`));
