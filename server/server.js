a6ea0385056d8fbdd13d97bbb2c51d709c449140
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-27 15:56:42 +0100

    init() Creating chat app
    First commit with the chat project structure
    
    S9,L106

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = process.env.POST || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.listen(PORT, () => console.log(`Started on port ${PORT}`));
